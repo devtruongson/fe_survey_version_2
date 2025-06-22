ab5a3cd79a1733dea671b85b3d1a2260eb2ec8f8
Truong Son Dev <truongsonpt.80@gmail.com>
2025-06-22 15:36:20 +0700

    updateupdate

---
+++ src/app/appSlice.ts
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const appSlice = createSlice({
+  name: 'appSlice',
+  initialState: {
+    value: 0
+  },
+  reducers: {
+    
+  },
+})
+
+export const { } = appSlice.actions
+export default appSlice.reducer
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,23 @@
 // import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
 import App from "./App.tsx";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./libs/query.ts";
+import { store, persistor } from "./app/store.ts";
 
 createRoot(document.getElementById("root")!).render(
     // <StrictMode>
     // </StrictMode>
-    <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <App />
-    </QueryClientProvider>
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <QueryClientProvider client={queryClient}>
+                <ReactQueryDevtools initialIsOpen={false} />
+                    <App />
+            </QueryClientProvider>
+        </PersistGate>
+    </Provider>
 );
